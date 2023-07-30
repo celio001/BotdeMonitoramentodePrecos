e5fd9f953ebc7565c1b2f057f30353c617d554cd
Celio <pessoalcelio01@gmail.com>
2023-07-29 23:48:04 -0300

    Abrindo navegador

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-
 
 def iniciar_driver():
     chrome_options = Options()
@@ -17,6 +17,11 @@ def iniciar_driver():
 
     })
     driver = webdriver.Chrome(service=ChromeService(
-        ChromeDriverManager().install()), options=chrome_options)
+        ChromeDriverManager(version='114.0.5735.90').install()), options=chrome_options)
 
     return driver
+
+driver = iniciar_driver()
+driver.get('https://www.amazon.com.br/s?k=Celular&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1YIHDLNNPWJHI&sprefix=celular%2Caps%2C190&ref=nb_sb_noss_2')
+
+input('digite para fechar')
